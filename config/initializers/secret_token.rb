fe76835d289f6abb00ad117fd3a3d991a8c8b2ff
sanman <learnsanjeev@gmail.com>
2013-05-05 16:56:36 +0530

    created rails app and link module scaffold with db migrate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handover::Application.config.secret_token = '309a52dfb828e9df862741f1f46efd462b771e5aa1942e423501bcbf85d0415ada1504c057453ee1200ddcb0298b395e389c8c8be43145ed634991fce7623a93'
